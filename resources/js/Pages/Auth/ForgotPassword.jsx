32ca16972e7ae4ad0c727adfe2bf321157fc05c9
jiawon <150703170+jiawon@users.noreply.github.com>
2025-02-28 17:20:20 +0800

    feat;login and password

--- resources/js/Pages/Auth/ForgotPassword.jsx
+++ resources/js/Pages/Auth/ForgotPassword.jsx
@@ -3,7 +3,7 @@ import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
 import GuestLayout from '@/Layouts/GuestLayout';
 import { Head, useForm } from '@inertiajs/react';
-
+import InputLabel from '@/Components/InputLabel';
 export default function ForgotPassword({ status }) {
     const { data, setData, post, processing, errors } = useForm({
         email: '',
@@ -16,40 +16,69 @@ export default function ForgotPassword({ status }) {
     };
 
     return (
-        <GuestLayout>
-            <Head title="Forgot Password" />
+        <div className="h-screen flex flex-col items-center justify-center bg-gray-100">
+            <div className="w-full flex items-center justify-between p-5 bg-white shadow">
+              
+                    <button className="flex items-center gap-[15px]" 
+                        onClick={() => window.location.href = route('login')}
+                        >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                            <path d="M25.3327 16H6.66602M6.66602 16L15.9993 25.3333M6.66602 16L15.9993 6.66667" stroke="#161B26" strokeWidth="1.1" strokeLinecap="round" strokeLinejoin="round"/>
+                        </svg>
+                        <span className="text-black text-center font-manrope text-[16px] not-italic font-normal leading-[22px]">Back to log in</span>
+                    </button>
 
-            <div className="mb-4 text-sm text-gray-600">
-                Forgot your password? No problem. Just let us know your email
-                address and we will email you a password reset link that will
-                allow you to choose a new one.
+                    <div>
+                        <img src="/assets/image/Header icon.svg" alt="icon" />
+                    </div>
+               
             </div>
+            <div className="flex items-center justify-center flex-1 w-full">
+                <form onSubmit={submit}
+                    className="w-[432px] p-10 flex flex-col items-center border rounded-md border-gray-300 shadow-lg bg-white"
+                    >
+                    {status && (
+                        <div className="mb-4 text-sm font-medium text-green-600">
+                            {status}
+                        </div>
+                        )}
+                    <div className="flex flex-col gap-8 w-full">
+                        <div className="flex flex-col w-full text-center not-italic self-stretch">
+                           <div className='text-vulcan-900 flex flex-col gap-2 font-sans text-[32px] font-medium leading-[38px]'>
+                            Forgot Password?
+                            <span className=' text-vulcan-700 font-manrope text-sm font-normal leading-5'>
+                                 Enter your email and we’ll send instruction to reset your password
+                            </span>
+                           </div>
+                        </div>
+                        <div className='flex flex-col w-full'>
+                            <div className="flex items-center">
+                                <InputLabel htmlFor="email" value="Email Address" />
+                                <span className="text-error-700">*</span>
+                            </div>
+                            <TextInput
+                                id="email"
+                                type="email"
+                                name="email"
+                                value={data.email}
+                                className="mt-1 block w-full"
+                                isFocused={true}
+                                onChange={(e) => setData('email', e.target.value)}
+                            />
 
-            {status && (
-                <div className="mb-4 text-sm font-medium text-green-600">
-                    {status}
-                </div>
-            )}
+                            <InputError message={errors.email} className="mt-2" />
 
-            <form onSubmit={submit}>
-                <TextInput
-                    id="email"
-                    type="email"
-                    name="email"
-                    value={data.email}
-                    className="mt-1 block w-full"
-                    isFocused={true}
-                    onChange={(e) => setData('email', e.target.value)}
-                />
+                        </div>
+                            <div className="flex ">
+                                <PrimaryButton className="flex w-full items-center justify-center text-vulcan-25 font-manrope text-sm not-italic font-medium leading-5" disabled={processing}>
+                                   Send
+                                </PrimaryButton>
+                            </div>
+                    </div>
+                </form>
+            </div>
+                    
 
-                <InputError message={errors.email} className="mt-2" />
-
-                <div className="mt-4 flex items-center justify-end">
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Email Password Reset Link
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
+        </div>
     );
 }
--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -3,7 +3,8 @@ import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
-import GuestLayout from '@/Layouts/GuestLayout';
+
+
 import { Head, Link, useForm } from '@inertiajs/react';
 
 export default function Login({ status, canResetPassword }) {
@@ -15,86 +16,108 @@ export default function Login({ status, canResetPassword }) {
 
     const submit = (e) => {
         e.preventDefault();
-
         post(route('login'), {
             onFinish: () => reset('password'),
         });
     };
 
     return (
-        <GuestLayout>
-            <Head title="Log in" />
-
-            {status && (
-                <div className="mb-4 text-sm font-medium text-green-600">
-                    {status}
-                </div>
-            )}
-
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('email', e.target.value)}
-                    />
-
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="mt-4 block">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) =>
-                                setData('remember', e.target.checked)
-                            }
-                        />
-                        <span className="ms-2 text-sm text-gray-600">
-                            Remember me
-                        </span>
-                    </label>
-                </div>
-
-                <div className="mt-4 flex items-center justify-end">
-                    {canResetPassword && (
-                        <Link
-                            href={route('password.request')}
-                            className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                        >
-                            Forgot your password?
-                        </Link>
+        <div className="h-screen flex flex-col items-center justify-center bg-gray-100">
+            <div className="w-full absolute top-0 right-0 flex justify-end items-center p-5 bg-white shadow">
+                <img src="/assets/image/Header icon.svg" alt="icon" />
+            </div>
+            <div className="flex items-center justify-center w-full">
+                <form 
+                    onSubmit={submit} 
+                    className="w-[432px] p-10 flex flex-col items-center border rounded-md border-gray-300 shadow-lg bg-white"
+                >
+                    {status && (
+                        <div className="mb-4 text-sm font-medium text-green-600">
+                            {status}
+                        </div>
                     )}
 
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Log in
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
+                    <div className="flex flex-col gap-8 w-full">
+                        <div className="flex flex-col items-center gap-5 w-full">
+                            <img src="/assets/image/currenttechlogo.png" alt="logo" />
+                            <div className="text-center w-full">
+                                <span className="text-vulcan-900 font-sans text-[32px] font-medium">
+                                    Welcome <span className="text-vulcan-500">Back!</span>
+                                </span>
+                                <span className="block text-sm font-manrope font-normal">Admin portal</span>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col gap-4 w-full">
+                            <div className="flex flex-col w-full">
+                                <div className="flex items-center">
+                                    <InputLabel htmlFor="email" value="Email" />
+                                    <span className="text-error-700">*</span>
+                                </div>
+                                <TextInput
+                                    id="email"
+                                    type="email"
+                                    name="email"
+                                    value={data.email}
+                                    className="mt-1 w-full"
+                                    autoComplete="username"
+                                    isFocused={true}
+                                    onChange={(e) => setData('email', e.target.value)}
+                                />
+                                <InputError message={errors.email} className="mt-2" />
+                            </div>
+
+
+                            <div className="flex flex-col w-full">
+                                <div className="flex items-center">
+                                    <InputLabel htmlFor="password" value="Password" />
+                                    <span className="text-error-700">*</span>
+                                </div>
+                                <TextInput
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    value={data.password}
+                                    className="mt-1 block w-full"
+                                    autoComplete="current-password"
+                                    onChange={(e) => setData('password', e.target.value)}
+                                />
+                                <InputError message={errors.password} className="mt-2" />
+                            </div>
+
+
+                            <div className="w-full flex items-center justify-between">
+                                <label className="flex items-center gap-2">
+                                    <Checkbox
+                                        name="remember"
+                                        checked={data.remember}
+                                        onChange={(e) => setData('remember', e.target.checked)}
+                                    />
+                                    <span className="font-manrope not-italic font-medium leading-[18px] text-xs text-vulcan-700">Remember Me</span>
+                                </label>
+
+                                {canResetPassword && (
+                                    <Link
+                                        href={route('password.request')}
+                                        className="text-xs text-vulcan-950 font-manrope not-italic font-medium leading-5 underline hover:text-gray-900"
+                                    >
+                                        Forgot Password?
+                                    </Link>
+                                )}
+                            </div>
+                        </div>
+
+                        <PrimaryButton className="w-full justify-center" disabled={processing}>
+                            Log in
+                        </PrimaryButton>
+                    </div>
+                </form>
+            </div>
+
+            {/* Footer */}
+            <div className="absolute bottom-4 text-center text-vulcan-400 font-manrope text-xs">
+                Copyright © Current Tech Industries 2022. All rights reserved.
+            </div>
+        </div>
     );
 }
