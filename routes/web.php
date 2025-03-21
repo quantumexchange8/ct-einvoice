186146bbe180f84cf9635abc0b988b203b52f99a
darren <darren.kooyz@gmail.com>
2025-03-21 16:48:36 +0800

    fix: route

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
-use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ConfigurationController;
 use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\InvoiceListingController;
 use App\Http\Controllers\MerchantController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
